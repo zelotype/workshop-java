4e3baf970845188def20a1d1fc5ef099436a9305
Kawisara bunyen <kawisara@Kawisaras-MacBook-Pro.local>
2021-08-03 14:31:52 +0700

    Add Workshop11

--- src/main/java/testdouble/GenerateIDService.java
+++ src/main/java/testdouble/GenerateIDService.java
@@ -3,9 +3,13 @@ package testdouble;
 import java.util.Random;
 
 public class GenerateIDService {
+    private Random random = new Random();
 
-    public String get() {
-        Random random = new Random();
+    public void setRandom(Random random) {
+        this.random = random;
+    }
+
+    public String get(Object name) {
         int number = random.nextInt(10);
         String id = "XYZ" + number;
 
--- src/test/java/testdouble/GenerateIDServiceTest.java
+++ src/test/java/testdouble/GenerateIDServiceTest.java
@@ -3,6 +3,8 @@ package testdouble;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GenerateIDServiceTest {
@@ -11,9 +13,50 @@ class GenerateIDServiceTest {
     @DisplayName("ID = XYZ7")
     public void case01() {
         GenerateIDService service = new GenerateIDService();
+        service.setRandom(new StubRandom(7));
 
-        String id = service.get();
-        
+        String id = service.get(null);
+
         assertEquals("XYZ7", id);
+    }
+
+    @Test
+    @DisplayName("Demo spy random :: Called nextInt()")
+    public void case02() {
+        GenerateIDService service = new GenerateIDService();
+        SpyRandom spyRandom = new SpyRandom();
+        service.setRandom(spyRandom);
+        service.get(null);
+        spyRandom.verify(1);
+    }
+
+
+}
+
+class SpyRandom extends Random {
+    private int counter = 0;
+
+    @Override
+    public int nextInt(int bound) {
+        this.counter++;
+        return 100000;
+    }
+
+    public void verify(int expectedCount) {
+        if (expectedCount != counter) {
+            fail("จำนวนการเรียกใช้งาน method nextInt() ไม่ถูกต้อง");
+        }
+    }
+}
+
+class StubRandom extends Random {
+    private final int number;
+    public StubRandom(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public int nextInt(int bound) {
+        return number;
     }
 }
